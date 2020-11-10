74263a45bd12eb7a16b53f852123f97944fa5a8a
Любимов Вадим <lyubimov_vyu@mail.ru>
2020-11-10 10:50:00 +0700

    Testing OOP program

--- program.py
+++ program.py
@@ -1,1 +1,21 @@
-print('Я домашка')
+class Human:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+
+    def output(self):
+        print(f"Меня зову {self.name}, мне {self.age} лет.")
+
+class Worker(Human):
+    def __init__(self, name, age, profeshion):
+        super().__init__(name, age)
+        self.profeshion=profeshion
+
+    def output(self):
+        print(f"Я {self.name}, мне {self.age} лет. Я работаю {self.profeshion}")
+
+maria=Human("Мария","18")
+sergey=Worker("Сергей","35","Строитель")
+
+maria.output()
+sergey.output()
